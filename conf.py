395ae54cc7a0ddb10d3a422b0d532f2fcd59ec8f
Troy Topnik <troyt@activestate.com>
2014-04-28 16:10:22 -0700

    Meant to leave this commented out

--- conf.py
+++ conf.py
@@ -153,7 +153,7 @@ html_static_path = ['_static']
 html_use_smartypants = False
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {'**': ['localtoc.html']}
+# html_sidebars = {'**': ['localtoc.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
